f6a27fdb5ab13eafa42649fd8e26396d7ed73b38
EduardoGuimaraes <edu.ardo.barros@hotmail.com>
2016-01-13 18:23:17 -0300

    Update netFlix.cpp

--- netFlix.cpp
+++ netFlix.cpp
@@ -4,15 +4,19 @@
 class Episodio{
 	string nome;
 	double duracao;
-	
+	int likes;
+	int dislikes;
+	int classificacao;
+};
+
+class Temporada{
+	int numero;
 };
 
 class Serie{
 	string nome;
 	string genero;
 	int favs;
-	int likes;
-	int dislikes;
-	int classificacao;
+	int numeroEpisodios;
 	
 };
